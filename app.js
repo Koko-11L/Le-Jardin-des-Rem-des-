fca7b9045b128bf7d64ae0b07be7ad102fc66576
Koko-11L <caroline.abouassaf.jit25@gmail.com>
2025-07-30 12:32:47 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+/*console.log("blabla");
+alert("Bienvenue sur mon site 😁");
+let prenom = prompt(" Quel est ton prénom ?");
+let nom = prompt(" Quel est ton prénom ?");
+
+console.log(" bonjour "+ prenom + " " + nom +"!");
+*/
+let monParagraphe = document.getElementById("animepara");
+
+
+function glowParagrapheIN() {
+    anime({
+        targets: "#animepara",
+        textShadow: [
+            "0 0 0px #fff", 
+            "0 0 15px #fff, 0 0 30px #fff, 0 0 45px #ff0000, 0 0 60px #ff0000, 0 0 75px #fff500"
+        ],
+        duration: 500,
+        easing: "easeInOutQuad"
+    });
+}
+
+function glowParagrapheOUT() {
+    anime({
+        targets: "#animepara",
+        textShadow: [
+            "0 0 15px #fff, 0 0 30px #fff, 0 0 45px #ff0000, 0 0 60px #ff0000, 0 0 75px #fff500",
+          "0 0 0px #fff"
+        ],
+        duration: 500,
+        easing: "easeInOutQuad"
+    });
+}
+
+
+monParagraphe.addEventListener("mouseover", glowParagrapheIN);
+monParagraphe.addEventListener("mouseout", glowParagrapheOUT);
